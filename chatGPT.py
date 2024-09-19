62ac4f5d6c0cfcbb43ca472235b385d15ea15042
Sanny Prajapati <101635131+darkuser21@users.noreply.github.com>
2024-09-19 20:00:19 +0530

    Add files via upload

---
+++ chatGPT.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from groq import Groq
+
+api = st.secrets["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"]
+client = Groq(api_key=api)
+
+st.title("SASTA chatGPT ")
+
+def chatGPT():
+    user_question = st.text_input("ESKE NICHE LPUCH JO PUCHNA HAI: ", "")
+    
+    if st.button("MUZE TOUCH KAR", key="ask_button"):
+        if user_question.strip() == "":
+            st.write("JALDI PUCH BHAI .")
+        else:
+            message = {
+                "content": user_question,
+                "role": "user"
+            }
+            
+            try:
+                chat_completion = client.chat.completions.create(
+                    messages=[message],
+                    model="llama3-8b-8192",
+                )
+                
+                if chat_completion.choices:
+                    response = chat_completion.choices[0].message.content
+                    st.text_area("ChatGPT:", value=response, height=200)
+                else:
+                    st.write("ChatGPT: KUCH GADBAD HAI")
+            
+            except Exception as e:
+                st.write(f"Error occurred: {e}")
+chatGPT()
