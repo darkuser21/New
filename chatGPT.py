f95d435caacf2885e1953be692fe89fe64cbaeed
Sanny Prajapati <101635131+darkuser21@users.noreply.github.com>
2024-09-19 21:59:34 +0530

    Update chatGPT.py

--- chatGPT.py
+++ chatGPT.py
@@ -4,10 +4,10 @@ from groq import Groq
 api = st.secrets["GROQ_API_KEY"]
 client = Groq(api_key=api)
 
-st.title("SASTA chatGPT ")
+st.title("SASTA chatGPT💀 ")
 
 def chatGPT():
-    user_question = st.text_input("ESKE NICHE LPUCH JO PUCHNA HAI: ", "")
+    user_question = st.text_input("ESKE NICHE PUCH JO PUCHNA HAI: ", "")
     
     if st.button("MUZE TOUCH KAR", key="ask_button"):
         if user_question.strip() == "":
@@ -26,7 +26,7 @@ def chatGPT():
                 
                 if chat_completion.choices:
                     response = chat_completion.choices[0].message.content
-                    st.text_area("YE LE ANPAD:", value=response, height=500)
+                    st.text_area("YE LE ANPAD💀:", value=response, height=500)
                 else:
                     st.write("ChatGPT: KUCH GADBAD HAI")
             
